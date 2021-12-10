0df86254164be7fd080f64602f280a8d83660acb
dialloflatno <brahimdiallo23@gmail.com>
2021-12-10 03:36:09 -0500

    router-css

--- src/Navigator.js
+++ src/Navigator.js
@@ -1,36 +1,32 @@
 import AddNew from './AddNew';
 import './App.css';
 import Search from './Search';
+import {Link} from "react-router-dom"
+import App from './App'
+
 
 function Navigator() {
-  
-  
-  // const [pokemonDex, setPokemonDex] = useState([])
-
-
-
-  // useEffect(() => {
-  //   fetch(url)
-  //   .then(res => res.json())
-  //   .then(pokemonData => setPokemonDex(pokemonData))
-  // }, [])
-  
-
-  // function handleSearch(inputSearch) {
-  //   const searchResults = pokemonDex.filter(pokeCard => pokeCard.description.toLowerCase().includes(inputSearch.toLowerCase()))
-  //   return (setSearchRes(searchResults))
-  // }
-
-
-
-  return (
+return (
     <div className="Navigator">
       <div className = " navContain">
-        <div className="buttonContainer">
-         </div>
+      <Link to='/App'>
+        <button className = "phoneHome">
+          <div className = "innerCircle"/>
+          ""
+          <div className = "inCircle"/>
+        </button>
+      </Link>
        <Search  className = "Search"/>
-         </div>
+       {/* <Search handleSearch = {handleSearch} className = "Search"/> */}
+      </div>  
+      <Link to='/AddNew'>
+        <button className= "ADDPOKE" aria-label = "+ ADD POKEMON">
+        + ADD NEW POKEMON
+        </button>
+      </Link>
+        {/* <AddNew /> */}
     </div>
+
   );
 }
 
