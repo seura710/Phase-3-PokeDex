a08d60255110c671f8b7c1efa8a2752e38c5485e
dialloflatno <brahimdiallo23@gmail.com>
2021-12-10 12:53:34 -0500

    lastest PokeDex

--- src/Form.js
+++ src/Form.js
@@ -4,16 +4,26 @@ import img from './placepoke.png'
 function Form() {
 
 
+const handleSubmit = (e) => 
+e.preventDefault;
+
+
 
   return (
     <div className="form">
-      <form >
-      <div className = "holdImg">
-        <img  className = "holdImg" aria-checked  alt = "placePoke" src = {img}/>
-         </div>
-          <div className ="formHolder">
-            <hr className ="vl"/>
+
+      <form  onSubmit = {handleSubmit} >
+   
             <div className ="formHolder">
+      <div className = "fixed"
+      >
+        <img  className = "imgADDNEW" aria-checked  alt = "placePoke" src = {img}/>
+          <div >
+            <div className = "block">
+            <hr className ="vl"/>
+            </div>
+         </div >
+         <div className ="f.contain">
             <span className="formGreet">YOU FOUND A NEW POKEMON?</span>
               <input placeholder ="Image"onChange ={(e) => console.log(e.target.value)} className ="newPokemon"/>
               <input placeholder ="Name" onChange ={(e) => console.log(e.target.value)} className ="newPokemon"/>
@@ -22,6 +32,7 @@ function Form() {
               <span> <input type ="number" placeholder = "Height" className ="nPHW" />
               <input type ="number" placeholder = "Weigth" className ="nPHW"/></span>
               <button className = "addPoke">ADD POKEMON</button>
+              </div>
               </div>
         </div>
       </form>
